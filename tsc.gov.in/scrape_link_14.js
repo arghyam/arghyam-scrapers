ad6e36cf1a1f1578b69a146e2274140c97f72bef
pravin <pravin.v@gramener.com>
2012-12-18 19:11:46 +0530

    drill down till district level

--- tsc.gov.in/scrape_link_14.js
+++ tsc.gov.in/scrape_link_14.js
@@ -3,9 +3,16 @@ var x       = require('casper').selectXPath,
     write   = require('./csv').write;
 
 var stateSel   = '#ctl00_ContentPlaceHolder1_Div1 ul:gt(0) li a';
-var stateTbSel = '#ctl00_ContentPlaceHolder2_divData table tbody:eq(1) tr:gt(0)';
+var stateTbSel = '#ctl00_ContentPlaceHolder2_divData table tbody:eq(1) tr:gt(0),#ctl00_ContentPlaceHolder2_divData table tbody:eq(2) tr:gt(0)';
 var stateList;
-var buffer     = [];
+
+var districtSel = '#ctl00_ContentPlaceHolder1_Div1 ul:gt(0) li a'
+var districtTbSel = '#ctl00_ContentPlaceHolder2_div_Data table:eq(0) tr:gt(4) td:nth-child(2), #ctl00_ContentPlaceHolder2_div_Data table:eq(3) tr:gt(1):lt(9) td:nth-child(3),#ctl00_ContentPlaceHolder2_div_Data table:eq(3) tr:gt(1):lt(9) td:nth-child(4),#ctl00_ContentPlaceHolder2_div_Data table:eq(3) tr:gt(1):lt(9) td:nth-child(5)'
+var districtData;
+var districtList;
+
+var buffer_state     = [];
+var buffer_district     = [];
 
 casper.start('http://tsc.gov.in/NBA/NBAHome.aspx', function()
 {
@@ -37,10 +44,51 @@ casper.start('http://tsc.gov.in/NBA/NBAHome.aspx', function()
         stateTbSel : stateTbSel,
         state      : state
       });
-      buffer.push.apply(buffer, stateData);
+      buffer_state.push.apply(buffer_state, stateData);
       console.log('State '+ (index + 1) +' out of '+stateList.length+' completed');
     });
     
+    // district level ---->
+    this.then(function()
+    {
+      districtList = this.evaluate(function(districtSel)
+      {
+        return $(districtSel).map(function(){ return [[ $(this).attr('id'),$(this).text().trim() ]] }).get();
+      },
+      {
+        districtSel : districtSel
+      });
+      this.each(districtList, function(casper, district, index)
+      {
+        this.then(function()
+        {
+          this.click(x('//*[@id="'+ district[0] +'"]'));
+        });
+        this.then(function()
+        {
+          districtData = this.evaluate(function(districtTbSel, district)
+          {
+            var rows = $(districtTbSel);
+            rows = rows.map(function(){ return $(this).children().map(function(){ return $(this).text().trim() }).get().slice(1); }).get();
+            rows.splice(0,0, district[1])
+            return [rows];
+          },
+          {
+            districtTbSel : districtTbSel,
+            district      : district
+          });
+          
+          buffer_district.push.apply(buffer_district, districtData);
+          console.log('District '+ (index + 1) +' out of '+districtList.length+' completed');
+        });
+        this.then(function()
+        {
+          this.back();
+        });
+      });
+    });
+    // <----
+
     this.then(function()
     {
       this.back();
@@ -49,7 +97,11 @@ casper.start('http://tsc.gov.in/NBA/NBAHome.aspx', function()
 
   this.then(function()
   {
-    write('stateData_L14.csv', buffer);
+    write('stateData_L14.csv', buffer_state);
+  });
+  this.then(function()
+  {
+    write('districtData_L14.csv', buffer_state);
   });
 });
 
