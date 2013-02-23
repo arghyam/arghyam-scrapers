e4bcbde3639f1c18b25daac7d086a2697147146b
S Anand <root.node@gmail.com>
2013-02-23 19:29:06 +0530

    Added source; added numbers to titles

--- stories.js
+++ stories.js
@@ -1,3 +1,12 @@
+var RYG = d3.scale.linear()
+    .clamp(true)
+    .domain([0, 0.5, 1])
+    .range(['#D73027', '#FFFFBF', '#1A9850']);
+
+var F = d3.format(',.0f'); // Float
+var N = d3.format(',.0f'); // Number == int
+var P = d3.format('.1%');  // Percent
+
 var stories = [
     {
         'menu'  : 'Financial Progress',
@@ -8,7 +17,7 @@ var stories = [
         'group' : ['State_Name', 'District_Name'],
         'size'  : function(d) { return d['Total_Projects_Outlay']; },
         'color' : function(d) { return d.depth == 3 ? RYG(d['ExpReported_Total'] / d['Total_Projects_Outlay']) : ''; },
-        'hover' : function(d) { return d['State_Name'] + ' - ' + d['District_Name']; },
+        'hover' : function(d) { return d['State_Name'] + ' - ' + d['District_Name'] + ': Total outlay: Rs ' + F(d['Total_Projects_Outlay']) + ' lakhs, Expenditure: ' + P(d['ExpReported_Total'] / d['Total_Projects_Outlay']); },
         'story' : 'Story to be written...'
     },
     {
@@ -20,7 +29,7 @@ var stories = [
         'group' : ['State_Name', 'District_Name'],
         'size'  : function(d) { return d['Total_Projects_Outlay']; },
         'color' : function(d) { return d.depth == 3 ? RYG(d['ExpReported_Center'] / d['ExpReported_Total']) : ''; },
-        'hover' : function(d) { return d['State_Name'] + ' - ' + d['District_Name']; },
+        'hover' : function(d) { return d['State_Name'] + ' - ' + d['District_Name'] + ': Total outlay: Rs ' + F(d['Total_Projects_Outlay']) + ' lakhs, Center: ' + P(d['ExpReported_Center'] / d['ExpReported_Total']); },
         'story' : 'Story to be written...'
     },
     {
@@ -32,7 +41,7 @@ var stories = [
         'group' : ['State_Name', 'District_Name'],
         'size'  : function(d) { return d['PO_IHHL_BPL']; },
         'color' : function(d) { return d.depth == 3 ? RYG(d['PP_IHHL_BPL'] / d['PO_IHHL_BPL']) : ''; },
-        'hover' : function(d) { return d['State_Name'] + ' - ' + d['District_Name']; },
+        'hover' : function(d) { return d['State_Name'] + ' - ' + d['District_Name'] + ': Planned BPL toilets: ' + N(d['PO_IHHL_BPL']) + ', Built: ' + P(d['PP_IHHL_BPL'] / d['PO_IHHL_BPL']); },
         'story' : 'Story to be written...'
     },
     {
@@ -44,7 +53,7 @@ var stories = [
         'group' : ['State_Name', 'District_Name'],
         'size'  : function(d) { return d['PO_IHHL_APL']; },
         'color' : function(d) { return d.depth == 3 ? RYG(d['PP_IHHL_APL'] / d['PO_IHHL_APL']) : ''; },
-        'hover' : function(d) { return d['State_Name'] + ' - ' + d['District_Name']; },
+        'hover' : function(d) { return d['State_Name'] + ' - ' + d['District_Name'] + ': Planned APL toilets: ' + N(d['PO_IHHL_APL']) + ', Built: ' + P(d['PP_IHHL_APL'] / d['PO_IHHL_APL']); },
         'story' : 'Story to be written...'
     }
 ];
