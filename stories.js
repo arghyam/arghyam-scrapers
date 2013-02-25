a4aecb29f95dcb79ec2550da095489b121cd1dba
S Anand <root.node@gmail.com>
2013-02-25 10:27:13 +0530

    Two-level treemap, and cleanup of hover text

--- stories.js
+++ stories.js
@@ -3,57 +3,84 @@ var color = d3.scale.linear()
     .domain([0, 0.5, 1, 2])
     .range(['#D73027', '#FFFFBF', '#1A9850', '#000']);
 
+// Display formats
 var F = d3.format(',.0f'); // Float
 var N = d3.format(',.0f'); // Number == int
 var P = d3.format('.1%');  // Percent
 
+function sum(d, metric) {
+    return d.depth == 2 ? d[metric] :
+           d.depth == 1 ? d3.sum(d.values, function(v) { return v[metric]; })
+                        : 0;
+}
+
+function hover_text(story) {
+  return function(d) {
+    var prefix = d.depth == 2 ? d['State_Name'] + ' - ' + d['District_Name'] + ': ' :
+                 d.depth == 1 ? d['key'] + ': '
+                              : '';
+    var num = sum(d, story.num[1]);
+    var den = sum(d, story.den[1]);
+    var size = sum(d, story.area[1]);
+    return (prefix +
+        story.area[0] + ' = ' + N(size) + '. ' +
+        story.num[0] + ' / ' + story.den[0] + ' = ' +
+        N(num) + ' / ' + N(den) + ' = ' + P(num / den)
+    );
+  };
+}
+
+function storify(story) {
+    story.size = function(d) { return d[story.area[1]]; };
+    story.filter = function(d) { return (d.Date == '10-12-2012') && (!d.District_Name.match(/^Total/)); };
+    story.color = function(d) { return color(sum(d, story.num[1]) / sum(d, story.den[1])); };
+    story.hover = hover_text(story);
+    return story;
+}
+
 var stories = [
-    {
+    storify({
         'menu'  : 'Financial Progress',
         'title' : 'Project outlay used',
         'file'  : 'data/tsc.gov.in/districtData_L1.csv',
         'url'   : 'http://tsc.gov.in/tsc/Report/Financial/RptFinancialProgressStatewiseDistrictwise.aspx?id=Home',
-        'filter': function(d) { return (d.Date == '10-12-2012') && (!d.District_Name.match(/^Total/)); },
-        'group' : ['State_Name', 'District_Name'],
-        'size'  : function(d) { return d['Total_Projects_Outlay']; },
-        'color' : function(d) { return d.depth == 3 ? color(d['ExpReported_Total'] / d['Total_Projects_Outlay']) : ''; },
-        'hover' : function(d) { return d['State_Name'] + ' - ' + d['District_Name'] + ': Total outlay: Rs ' + F(d['Total_Projects_Outlay']) + ' lakhs, Expenditure: ' + P(d['ExpReported_Total'] / d['Total_Projects_Outlay']); },
+        'group' : ['State_Name'],
+        'area'  : ['Total outlay', 'Total_Projects_Outlay'],
+        'num'   : ['Total expenses', 'ExpReported_Total'],
+        'den'   : ['Total outlay', 'Total_Projects_Outlay'],
         'story' : 'Story to be written...'
-    },
-    {
+    }),
+    storify({
         'menu'  : 'Financial Progress',
         'title' : 'Share of centre',
         'file'  : 'data/tsc.gov.in/districtData_L1.csv',
         'url'   : 'http://tsc.gov.in/tsc/Report/Financial/RptFinancialProgressStatewiseDistrictwise.aspx?id=Home',
-        'filter': function(d) { return (d.Date == '10-12-2012') && (!d.District_Name.match(/^Total/)); },
-        'group' : ['State_Name', 'District_Name'],
-        'size'  : function(d) { return d['Total_Projects_Outlay']; },
-        'color' : function(d) { return d.depth == 3 ? color(d['ExpReported_Center'] / d['ExpReported_Total']) : ''; },
-        'hover' : function(d) { return d['State_Name'] + ' - ' + d['District_Name'] + ': Total outlay: Rs ' + F(d['Total_Projects_Outlay']) + ' lakhs, Center: ' + P(d['ExpReported_Center'] / d['ExpReported_Total']); },
+        'group' : ['State_Name'],
+        'area'  : ['Total outlay', 'Total_Projects_Outlay'],
+        'num'   : ['Centre expenses', 'ExpReported_Center'],
+        'den'   : ['Total expenses', 'ExpReported_Total'],
         'story' : 'Story to be written...'
-    },
-    {
+    }),
+    storify({
         'menu'  : 'Physical Progress',
         'title' : '% toilets built below poverty household',
         'file'  : 'data/tsc.gov.in/districtData_L4.csv',
         'url'   : 'http://tsc.gov.in/tsc/Report/Physical/RptPhysicalProgessStateWiseDistrictwise.aspx?id=Home',
-        'filter': function(d) { return (d.Date == '10-12-2012') && (!d.District_Name.match(/^Total/)); },
-        'group' : ['State_Name', 'District_Name'],
-        'size'  : function(d) { return d['PO_IHHL_BPL']; },
-        'color' : function(d) { return d.depth == 3 ? color(d['PP_IHHL_BPL'] / d['PO_IHHL_BPL']) : ''; },
-        'hover' : function(d) { return d['State_Name'] + ' - ' + d['District_Name'] + ': Planned BPL toilets: ' + N(d['PO_IHHL_BPL']) + ', Built: ' + P(d['PP_IHHL_BPL'] / d['PO_IHHL_BPL']); },
+        'group' : ['State_Name'],
+        'area'  : ['Planned BPL toilets', 'PO_IHHL_BPL'],
+        'num'   : ['Built BPL toilets', 'PP_IHHL_BPL'],
+        'den'   : ['Planned BPL toilets', 'PO_IHHL_BPL'],
         'story' : 'Story to be written...'
-    },
-    {
+    }),
+    storify({
         'menu'  : 'Physical Progress',
         'title' : '% toilets built above poverty household',
         'file'  : 'data/tsc.gov.in/districtData_L4.csv',
         'url'   : 'http://tsc.gov.in/tsc/Report/Physical/RptPhysicalProgessStateWiseDistrictwise.aspx?id=Home',
-        'filter': function(d) { return (d.Date == '10-12-2012') && (!d.District_Name.match(/^Total/)); },
-        'group' : ['State_Name', 'District_Name'],
-        'size'  : function(d) { return d['PO_IHHL_APL']; },
-        'color' : function(d) { return d.depth == 3 ? color(d['PP_IHHL_APL'] / d['PO_IHHL_APL']) : ''; },
-        'hover' : function(d) { return d['State_Name'] + ' - ' + d['District_Name'] + ': Planned APL toilets: ' + N(d['PO_IHHL_APL']) + ', Built: ' + P(d['PP_IHHL_APL'] / d['PO_IHHL_APL']); },
+        'group' : ['State_Name'],
+        'area'  : ['Planned APL toilets', 'PO_IHHL_APL'],
+        'num'   : ['Built APL toilets', 'PP_IHHL_APL'],
+        'den'   : ['Planned APL toilets', 'PO_IHHL_APL'],
         'story' : 'Story to be written...'
-    }
+    })
 ];
