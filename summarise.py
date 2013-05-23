78d12b1587c3a5f6a8f79a61470d4bf1f07e72b3
S Anand <root.node@gmail.com>
2013-05-23 08:55:53 +0530

    Added script to merge datasets into a single data.csv

---
+++ summarise.py
@@ -0,0 +1,42 @@
+import os
+import csv
+import sys
+
+keys = ('State_Name', 'District_Name')
+
+def latest(path):
+    """Return data with non-empty State_Name, District_Name as keys"""
+    result = {}
+    for row in csv.DictReader(open(path)):
+        key = tuple(row[v].strip() for v in keys)
+
+        # Ignore Total rows
+        if not all(key) or any(k.lower().find('total') >= 0 for k in key):
+            continue
+        result[key] = row
+
+    return result
+
+
+root = 'tsc.gov.in'
+files = {}
+result = {}
+fields = []
+for column in csv.DictReader(open('summarise.csv')):
+    if not column['File']:
+        continue
+
+    if column['File'] not in files:
+        print column['File']
+        path = os.path.join(root, column['File'])
+        files[column['File']] = latest(path)
+
+    col = column['Column']
+    fields.append(col)
+    for index, row in files[column['File']].iteritems():
+        result.setdefault(index, {})[col] = row[col]
+
+out = csv.writer(open('data.csv', 'w'), lineterminator='\n')
+out.writerow(fields)
+for index in sorted(result):
+    out.writerow([result[index].get(f, '').strip() for f in fields])
