ae15eb7e6e06dfd8fd0cdd00e54d44e0d648ca7a
S Anand <root.node@gmail.com>
2013-05-23 12:46:45 +0530

    Add a row that notes the date on which the column was extracted

--- summarise.py
+++ summarise.py
@@ -7,6 +7,7 @@ keys = ('State_Name', 'District_Name')
 def latest(path):
     """Return data with non-empty State_Name, District_Name as keys"""
     result = {}
+    date = None
     for row in csv.DictReader(open(path)):
         key = tuple(row[v].strip() for v in keys)
 
@@ -14,12 +15,14 @@ def latest(path):
         if not all(key) or any(k.lower().find('total') >= 0 for k in key):
             continue
         result[key] = row
+        date = row['Date']
 
-    return result
+    return date, result
 
 
 root = 'tsc.gov.in'
 files = {}
+dates = {}
 result = {}
 fields = []
 for column in csv.DictReader(open('summarise.csv')):
@@ -29,12 +32,14 @@ for column in csv.DictReader(open('summarise.csv')):
     if column['File'] not in files:
         print column['File']
         path = os.path.join(root, column['File'])
-        files[column['File']] = latest(path)
+        dates[column['File']], files[column['File']] = latest(path)
 
     col = column['Column']
     fields.append(col)
     for index, row in files[column['File']].iteritems():
         result.setdefault(index, {})[col] = row[col]
+
+    result.setdefault(('_Updated', ''), {})[col] = dates[column['File']]
 
 out = csv.writer(open('data.csv', 'w'), lineterminator='\n')
 out.writerow(fields)
