d58c2039424bd2aa150d382385766360862baa52
S Anand <root.node@gmail.com>
2013-05-23 07:35:55 +0530

    BUGFIX: district selector had a limit of only 30 districts

--- tsc.gov.in/PercentageFinComponentStatewiseDistrictwise_net.js
+++ tsc.gov.in/PercentageFinComponentStatewiseDistrictwise_net.js
@@ -7,12 +7,12 @@ var stateTbId           = "ctl00_ContentPlaceHolder1_divStateWise",
     stateTbIds;
 
 var districtTbId  = 'ctl00_ContentPlaceHolder1_divDistrictWise',
-    districtTbSel = '#'+ districtTbId +' tbody tbody tr:gt(3):lt(31)';
+    districtTbSel = '#'+ districtTbId +' tbody tbody tr:gt(3)';
 
 var buffer  =  [];
 
 casper.start('http://tsc.gov.in/tsc/Report/Financial/RptPercentageFinComponentStatewiseDistrictwise_net.aspx?id=FIN', function() {
-  stateTbIds    = this.evaluate(function(stateTbId) {
+  stateTbIds = this.evaluate(function(stateTbId) {
     return $('#'+ stateTbId +'').find('a').map(function() {
       return [[ $(this).attr('id'), $(this).text() ]];
     }).get();
@@ -26,9 +26,10 @@ casper.start('http://tsc.gov.in/tsc/Report/Financial/RptPercentageFinComponentSt
         if($(this).text() !== '') { return $(this).text().trim(); }
       }).get() ];
     }).get();
-    // <-- specific to this link
-    rows[ rows.length -1 ].splice(0,0,'');
-    // -->
+
+    // Insert blank in place of SL_No for Grand Total row
+    rows[rows.length - 1].splice(0, 0, '');
+
     return rows;
   }, {stateTbSel: stateTbSel});
 
@@ -38,23 +39,23 @@ casper.start('http://tsc.gov.in/tsc/Report/Financial/RptPercentageFinComponentSt
     write('PercentageFinComponentStatewiseDistrictwise_net.state.csv', stateData);
   });
 
-  this.each(stateTbIds, function(casper, stateID, index) {
+  this.each(stateTbIds.slice(0, 4), function(casper, stateID, index) {
     this.then(function() {
       this.click(x('//*[@id="' + stateID[0] + '"]'));
     });
     this.then(function() {
       // returns [[ANDHRA PRADESH, 7, KARIMNAGAR, 13-02-2003, 11/2012, 10849.48, 7165.48, 2612.85, 1071.15, 5602.87, 1943.23, 176.13, 7722.23, 4840.01, 1939.23, 176.13, 6955.37],..]
       districtData = this.evaluate(function(districtTbSel, stateName) {
-        // --->
         var rows = $(districtTbSel).not(':last');
-        // <---
+
         rows = rows.map(function(i) {
           var row = $(this).children().map(function() {
             if($(this).text() !== '') { return $(this).text().trim(); }
           }).get();
-          // --->
+
+          // Insert blank in place of SL_No for Total row
           if ((rows.length - 1) == i) { row.splice(0, 0, ''); }
-          // <---
+
           row.splice(0, 0, stateName);
           return [row];
         }).get();
