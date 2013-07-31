9fe9c074cc96ba42df362040a1ce71aa7e0b5e91
santhosh0409 <santhoshjegank@gmail.com>
2013-07-31 21:22:00 +0530

    Changed github.io to arghyam.github.io, staging page - testing

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var host = window.location.host;
 //var iwp = 'www.indiawaterportal.org';
-var iwp = 'github.io';
+var iwp = 'arghyam.github.io';
 console.log(host);
 d3.selectAll('.tooltip').remove();
 // Clicking on the home button...
