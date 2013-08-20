c52c3600b7b349656087de75c5188750d9fdccd4
santhosh0409 <santhoshjegank@gmail.com>
2013-08-20 11:57:23 +0530

    changed href for IWP.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var host = window.location.href;
-var iwp = 'http://arghyam.github.io/arghyam-scrapers/?'; 
+var iwp = 'http://www.indiawaterportal.org/data-apps/'; 
 console.log(host);
 d3.selectAll('.tooltip').remove();
 // Clicking on the home button...
