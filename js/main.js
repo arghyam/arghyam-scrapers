8e0451613d4d03fc40b51ce03b7d20d3a48d40ed
santhosh0409 <santhoshjegank@gmail.com>
2014-02-18 12:36:24 +0530

    changed to github mode

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-var host = window.location.host;
-var iwp = 'www.indiawaterportal.org';
-//var host = window.location.href;
+//var host = window.location.host;
+//var iwp = 'www.indiawaterportal.org';
+var host = window.location.href;
 //var iwp = 'http://arghyam.github.io/arghyam-scrapers/?#';
+var iwp = 'http://localhost/arghyam/arghyam-scrapers/?#';
 console.log(host);
 var slides = [];
 d3.selectAll('.tooltip').remove();
